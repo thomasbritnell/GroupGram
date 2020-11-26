485c4badc363d8fcdc866aa3c22d2070a100a294
edwin-do <doedwin123@gmail.com>
2020-11-25 21:26:51 -0500

    change breadcrumb color

--- frontend/src/components/Breadcrumb.js
+++ frontend/src/components/Breadcrumb.js
@@ -5,8 +5,8 @@ function Breadcrumb(){
         <div>
             <nav aria-label="breadcrumb">
             <ol class="breadcrumb bg-transparent">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item"><a href="#">Orange</a></li>
+                <li class="breadcrumb-item"><a className="text-light" href="#">Home</a></li>
+                <li class="breadcrumb-item"><a className="text-light" href="#">Orange</a></li>
                 <li class="breadcrumb-item active" aria-current="page">Pictures</li>
             </ol>
             </nav>
