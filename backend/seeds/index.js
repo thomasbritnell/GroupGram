04d41f99a95814563d942639337cf172adb9e08a
edwin-do <doedwin123@gmail.com>
2020-12-02 21:43:46 -0500

    attempt to add albumID

--- backend/seeds/index.js
+++ backend/seeds/index.js
@@ -49,7 +49,7 @@ const seedDB = async () => {
         postDate: "2020-08-03T21:12:23.964Z",
         albumName: "teal",
         group: "HS",
-        id: "0",
+        id: Object,
     })
 
     const album2 = new Album({
