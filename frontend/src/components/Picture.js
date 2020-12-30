4180ed515acd71e710fbb8c1a7b5dc47d217f7a9
edwin-do <doedwin123@gmail.com>
2020-12-29 19:03:26 -0500

    add menu icon

--- frontend/src/components/Picture.js
+++ frontend/src/components/Picture.js
@@ -1,5 +1,7 @@
 import { React, Component } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons'
 
 
 export default class Picture extends Component {
@@ -16,7 +18,10 @@ export default class Picture extends Component {
             <div className="card border border-dark mt-3">
                 <img className="card-img-top polaroid" src={this.props.url} alt="Card image cap" />
                 <div className="card-body">
-                    <p className="card-text">{this.props.caption}</p>
+                    <p className="card-text">
+                        {this.props.caption}
+                        <FontAwesomeIcon className="ml-2" icon={faEllipsisV} />
+                    </p>
                     <p>{this.props.album}</p>
                     <p>{this.props.user}</p>
                 </div>
